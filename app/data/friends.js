185010955dc473ecf1f51fbee7df0cc151cbd664
sanishjohn <sanishj@gmail.com>
2017-08-12 17:44:09 -0500

    incremental updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,39 @@
 var friends = [
-   {
-      "name":"Ashton Kutcher",
-      "photo":"./animals-photos01.jpg",
-      "scores":[4,5,4,4,5,1,2,5,4,1]
-   },
-   {
-      "name":"Kim Kardashian",
-      "photo":"./animals-photos02.jpg",
-      "scores":[4,1,5,1,3,2,2,1,3,2]
-   },
-   {
-      "name":"Bruce Willis",
-      "photo":"./animals-photos03.jpg",
-      "scores":[2,2,2,3,4,1,3,2,5,5]
-   },
-   {
-      "name":"Meg Ryan",
-      "photo":"./animals-photos04.jpg",
-      "scores":[3,3,4,5,2,1,3,2,2,3]
-   },
-	{
-	   "name":"Ben Affleck",
-	   "photo":"./animals-photos05.jpg",
-	   "scores":[4,3,4,1,5,1,5,3,1,4]
-	},
-      {
-      "name":"Marilyn Monroe",
-      "photo":"./animals-photos06.jpg",
-      "scores":[5,3,4,1,4,3,5,3,5,4]
-   },
-	{
-	   "name":"Tom Cruise",
-	   "photo":"./Funny-Animal-Pics-6.jpg",
-	   "scores":[1,4,2,5,2,5,3,2,4,5]
-	}
+    {
+        "name": "Ashton Kutcher",
+        "photo": "./animals-photos01.jpg",
+        "scores": [4, 5, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Kim Kardashian",
+        "photo": "./animals-photos02.jpg",
+        "scores": [4, 1, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Bruce Willis",
+        "photo": "./animals-photos03.jpg",
+        "scores": [2, 2, 2, 3, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Meg Ryan",
+        "photo": "./animals-photos04.jpg",
+        "scores": [3, 3, 4, 5, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Ben Affleck",
+        "photo": "./animals-photos05.jpg",
+        "scores": [4, 3, 4, 1, 5, 1, 5, 3, 1, 4]
+    },
+    {
+        "name": "Marilyn Monroe",
+        "photo": "./animals-photos06.jpg",
+        "scores": [5, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        "name": "Tom Cruise",
+        "photo": "./Funny-Animal-Pics-6.jpg",
+        "scores": [1, 4, 2, 5, 2, 5, 3, 2, 4, 5]
+    }
 
 ]
 
