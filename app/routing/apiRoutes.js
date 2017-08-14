db81f507301d8b37dc6451538f46ab2724bbdf22
sanishjohn <sanishj@gmail.com>
2017-08-13 21:21:47 -0500

    incremental updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,56 @@
-
-//require the friends file
+//require the friends data file
 var friends = require('../data/friends.js');
 
 //Routes
+module.exports = function(app){
+
+	// API GET Requests
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
+
+	// API POST Requests
+	app.post('/api/friends', function(req, res){
+
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDifference: 1000
+		};
+
+		var userData 	= req.body;
+		var userName 	= userData.name;
+		var userPhoto 	= userData.photo;
+		var userScores 	= userData.scores;
+
+		var totalDifference = 0;
+
+		// Loop through all the friend possibilities in the database. 
+		for  (var i=0; i< friends.length; i++) {
+			console.log(friends[i].name);
+			totalDifference = 0;
+
+			// Loop through all the scores of each friend
+			for (var j=0; j< friends[i].scores[j]; j++){
+
+				// logic to figure out the difference between the scores and sum them into the totalDifference
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+				// If the sum of differences is less then the differences of the current "best match"
+				if (totalDifference <= bestMatch.friendDifference){
+
+					// Reset the bestMatch to be the new friend. 
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+				}
+			}
+		}
+
+		friends.push(userData);
+
+		res.json(bestMatch);
+
+	});
+
+}
